ee4cca49ef9be33e4a8a25613b7a07713ea42602
amlanacharya <85425637+amlanacharya@users.noreply.github.com>
2021-06-11 11:11:23 +0530

    Delete python-sql connector basics directory

--- python-sql connector basics/connectToMysql.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as connection
-
-try:
-    mydb = connection.connect(host="localhost", user="root", passwd="mysql",use_pure=True)
-    # check if the connection is established
-    print(mydb.is_connected())
-    mydb.close()
-except Exception as e:
-    print(str(e))
